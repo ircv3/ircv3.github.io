d7c7392f47d4b121871f55bca26200bcff1e49d3
Daniel Oaks <daniel@danieloaks.net>
2021-05-23 07:20:31 +1000

    Fix checknetworks script

--- _data/checknetworks.py
+++ _data/checknetworks.py
@@ -12,7 +12,7 @@
 # <https://creativecommons.org/publicdomain/zero/1.0/>.
 
 # to run this script:
-#     pip3 install --update girc pyyaml docopt
+#     pip3 install --upgrade girc pyyaml docopt
 #     cd _data
 #     python3 checknetworks.py
 
@@ -24,40 +24,132 @@ Usage:
 Options:
     <supportfile>  YAML network list [default: su_networks.yml]."""
 from docopt import docopt
-import girc
 import yaml
+import os
+import ssl
+import socket
+import traceback
+from datetime import datetime
+from girc.ircreactor.envelope import RFC1459Message
+from girc.ircreactor.events import EventManager
+
+# settings
+nick = 'ircv3-tst'
+username = 'ircv3'
+realname = 'IRCv3 cap/isupport Tester'
+
+
+class IRCv3Connection:
+    """Connects to an IRC server at the given address."""
+    def __init__(self, hostname, nick, username, realname, ipv6=False, debug=False):
+        self.debug = debug
+        self._data = b''
+
+        if ipv6:
+            family = socket.AF_INET6
+        else:
+            family = socket.AF_INET
+
+        self.sock = socket.socket(family, socket.SOCK_STREAM)
+        context = ssl.create_default_context()
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
+        self.sock = context.wrap_socket(self.sock, server_hostname=hostname)
+        self.sock.connect((hostname, 6697))
+
+        # identify
+        self.send('CAP LS 302')
+        self.send('NICK {}'.format(nick))
+        self.send('USER {} 0 * :{}'.format(username, realname))
+        self.send('CAP END')
+
+        # events setup
+        self._events = EventManager()
+        self._events.register('irc message 005', self.on_005)
+        self._events.register('irc message cap', self.on_cap)
+        self._events.register('irc message ping', self.on_ping)
+        self._events.register('irc message 376', self.on_connected)
+        self._events.register('irc message 422', self.on_connected)
+
+        # basic data
+        self.caps = {}
+        self.isupport = {}
+    
+    def loop(self):
+        """Start connection loop."""
+        try:
+            while True:
+                line = self.recv()
+                if line.strip() == '':
+                    continue
+                m = RFC1459Message.from_message(line)
+                if m.verb == 'ERROR':
+                    break
+        except:
+            trace = traceback.format_exc()
+            print(trace)
+
+    def recv(self, bytes=4096):
+        """Receive any info from the IRC server."""
+        # recv existing lines before receiving new ones
+        if b'\n' not in self._data:
+            self._data += self.sock.recv(bytes)
+        if b'\n' in self._data:
+            raw_bytes, self._data = self._data.split(b'\n', maxsplit=1)
+            line = raw_bytes.decode('utf-8', 'ignore').strip('\r\n')
+            if line.strip() == '':
+                return ''
+            if self.debug:
+                print('{} <-  {}'.format(datetime.today().strftime('%H:%M:%S'), line))
+            m = RFC1459Message.from_message(line)
+            self._events.dispatch('irc message {}'.format(m.verb.lower()), m)
+            return line
+        return ''
+
+    def send(self, line):
+        """Send the given line to the IRC server."""
+        self.send_raw('{}\r\n'.format(line))
+
+    def send_raw(self, line):
+        """Send the given line to the IRC server."""
+        if self.debug:
+            print('{} -> {}'.format(datetime.today().strftime('%H:%M:%S'), line.strip('\r\n')))
+        self.sock.send(line.encode('utf-8', 'ignore'))
+
+    def on_ping(self, msg):
+        self.send('PONG :{}'.format(msg.params[-1]))
+
+    def on_cap(self, msg):
+        if msg.params[1].upper() != 'LS':
+            return
+        for cap in msg.params[-1].split():
+            value = ''
+            if '=' in cap:
+                cap, value = cap.split('=', maxsplit=1)
+            self.caps[cap] = value
+
+    def on_005(self, msg):
+        for name in msg.params[1:-1]:
+            value = ''
+            if '=' in name:
+                name, value = name.split('=', maxsplit=1)
+            self.isupport[name] = value
+
+    def on_connected(self, msg):
+        self.send('QUIT')
 
 
 def check_net(versions, info):
     print("Connecting to:", info['name'])
     hostname = info['net-address']['display']
 
-    reactor = girc.Reactor()
-
     supported = {}
 
-    # register events
-    @reactor.handler('in', 'endofmotd')
-    @reactor.handler('in', 'nomotd')
-    def handle_endofmotd(event):
-        # get caps
-        supported['caps'] = []
-        for name in event['server'].capabilities.available:
-            supported['caps'].append(name)
+    irc = IRCv3Connection(hostname, nick, username, realname, debug=True)
+    irc.loop()
 
-        # get features
-        supported['features'] = []
-        for name in event['server'].features.available:
-            supported['features'].append(name)
-
-        event['server'].quit()
-
-    # without TLS then with TLS
-    server = reactor.create_server(info['name'] + ' noTLS')
-    server.set_user_info('ircv3-tst', user='ircv3', real='IRCv3 tester')
-    server.connect(hostname, 6667, ssl=False)
-
-    reactor.run_forever()
+    supported['features'] = irc.isupport.keys()
+    supported['caps'] = irc.caps.keys()
 
     # generate cap lists
     all_caps = []
@@ -84,7 +176,7 @@ def check_net(versions, info):
             print(' *!* + ', info['name'], 'now also supports', cap)
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version=girc.__version__)
+    arguments = docopt(__doc__, version='0.0.1')
 
     if arguments['run']:
         print("NOTE: The output of this script can be incorrect due to nets only exposing certain caps when on/not on TLS, and other implementation and network-specific factors.")
